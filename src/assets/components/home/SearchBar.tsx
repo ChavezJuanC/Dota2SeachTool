ceca91257f119cfb40c750800cbe30db9a5922f8
SKRPTZ <dosomethingwild012712@gmail.com>
2025-05-15 01:29:45 -0600

    completed search bar with dynamic modifier menu

--- src/assets/components/home/SearchBar.tsx
+++ src/assets/components/home/SearchBar.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { BiSearch } from "react-icons/bi";
 
 function SearchBar() {
     const [searchValue, setSearchValue] = useState<string>("");
+    const [searchType, setSearchType] = useState<string>("player");
 
     function handleSearch() {
         // Handle the search logic here
@@ -9,28 +11,46 @@ function SearchBar() {
             console.log("Please enter a search term.");
             return;
         } else {
-            console.log("Searching for:", searchValue);
+            console.log(
+                "Searching for: ",
+                searchValue,
+                " in type: ",
+                searchType
+            );
         }
     }
 
     return (
-        <div id="search-bar-container">
-            <input
-                type="text"
-                id="search-bar"
-                placeholder="Search..."
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        handleSearch();
-                    }
-                }}
-            />
-            <button id="search-button" onClick={handleSearch}>
-                Search
-            </button>
-        </div>
+        <>
+            <div id="search-bar-container">
+                <input
+                    type="text"
+                    id="search-bar"
+                    placeholder="Search..."
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleSearch();
+                        }
+                    }}
+                />
+                <div>
+                    <select
+                        id="search-type-menu"
+                        value={searchType}
+                        onChange={(e) => setSearchType(e.target.value)}
+                    >
+                        <option value="player">Player</option>
+                        <option value="match">Match</option>
+                        <option value="hero">Hero</option>
+                    </select>
+                </div>
+                <div id="search-button" onClick={handleSearch}>
+                    <BiSearch id="search-icon" />
+                </div>
+            </div>
+        </>
     );
 }
 
