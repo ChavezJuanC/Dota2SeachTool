03bd5c3339affe410e853019362c41df9145124c
SKRPTZ <dosomethingwild012712@gmail.com>
2025-05-24 18:26:21 -0600

    implemented about section

--- src/assets/main_routes/About.tsx
+++ src/assets/main_routes/About.tsx
@@ -2,8 +2,29 @@ import BackHomeButton from "../components/general/BackHomeButton";
 
 function About() {
     return (
-        <div >
+        <div>
             <BackHomeButton />
+            <div id="about-section">
+                <h1 className="main-section-title">About</h1>
+                <h3 id="about-text">
+                    Welcome to my Dota 2 stats page — a simple, clean, and
+                    minimalistic tool for checking your most essential in-game
+                    stats.
+                    <br />
+                    <br />
+                    This project is heavily inspired by OpenDota.com, a powerful
+                    and comprehensive platform for Dota 2 players. My version is
+                    a more lightweight alternative, focused on the basics —
+                    perfect for casual or less competitive players who just want
+                    quick insights without the extra complexity.
+                    <br />
+                    <br />
+                    Special thanks to the creators and maintainers of the Dota 2
+                    API and the OpenDota API. This project wouldn’t be possible
+                    without their work and dedication to making Dota 2 data
+                    accessible to everyone.
+                </h3>
+            </div>
         </div>
     );
 }
