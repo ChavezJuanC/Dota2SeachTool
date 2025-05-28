3cf6a92b460814ae7f3fe96c813817dbc938c309
SKRPTZ <dosomethingwild012712@gmail.com>
2025-05-28 16:28:16 -0600

    Implemented drop-down for fileters UI and improved spacing

--- src/assets/components/general/HeroFeedByWinRate.tsx
+++ src/assets/components/general/HeroFeedByWinRate.tsx
@@ -3,11 +3,20 @@ import HeroFeedByWinRateCard from "./HeroFeedByWinRateCard";
 function HeroFeedByWinRate() {
     return (
         <div id="hero-feed-by-win-rate-container">
-            <h1 className="main-section-title" id="heroes-by-win-rate-little">
+            <h1 className="main-section-title" id="heroes-by-win-rate-title">
                 Heroes By Win Rate
             </h1>
+            <select name="filter-options" id="win-rate-filters">
+                <option value="turbo">Turbo</option>
+                <option value="progames">Pro Games</option>
+            </select>
             {/*Create a drop down menu here to sort by turbo or pro win rate*/}
             <div id="hero-feed-by-win-rate-cards-wrapper">
+                <HeroFeedByWinRateCard />
+                <HeroFeedByWinRateCard />
+                <HeroFeedByWinRateCard />
+                <HeroFeedByWinRateCard />
+                <HeroFeedByWinRateCard />
                 <HeroFeedByWinRateCard />
                 <HeroFeedByWinRateCard />
                 <HeroFeedByWinRateCard />
